5ef33ccbe3c469c876e07e9c4a4d4d9f0d070b81
Bullets-jyr <jyr@bullets.co.kr>
2023-11-02 00:51:54 +0900

    Initial commit

---
+++ lib/data/memory/vo_todo.dart
@@ -0,0 +1,26 @@
+import 'package:fast_app_base/data/local/collection/todo_db_model.dart';
+import 'package:fast_app_base/data/memory/todo_status.dart';
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'vo_todo.freezed.dart';
+part 'vo_todo.g.dart';
+
+@unfreezed
+class Todo with _$Todo{
+
+  Todo._();
+
+  factory Todo({
+    required final int id,
+    @JsonKey(name: 'created_time')required final DateTime createdTime,
+    DateTime? modifyTime,
+    required String title,
+    required DateTime dueDate,
+    @Default(TodoStatus.unknown) TodoStatus status,
+  }) = _Todo;
+
+  factory Todo.fromJson(Map<String, Object?> json) => _$TodoFromJson(json);
+
+  TodoDbModel get dbModel => TodoDbModel(id, createdTime, modifyTime, title, dueDate, status);
+}
---
+++ lib/data/remote/todo_api.dart
@@ -0,0 +1,64 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:fast_app_base/common/common.dart';
+import 'package:fast_app_base/data/remote/clients/todo_client.dart';
+import 'package:fast_app_base/data/remote/result/api_error.dart';
+
+import '../memory/vo_todo.dart';
+import '../simple_result.dart';
+import '../todo_repository.dart';
+
+///Remote DB
+class TodoApi implements TodoRepository<ApiError> {
+  //서버에서 Content-Type : application/json 헤더를 주지 않는 경우 아래 처럼 강제 변환 가능
+  //final client = TodoClient(Dio()..interceptors.add(DioJsonResponseConverter()));
+  final client = TodoClient(Dio(BaseOptions(
+      baseUrl: Platform.isAndroid ? 'http://10.0.2.2:8080/' : 'http://localhost:8080/')));
+
+  TodoApi._();
+
+  static TodoApi instance = TodoApi._();
+
+  @override
+  Future<SimpleResult<List<Todo>, ApiError>> getTodoList() async {
+    return tryRequest(() async {
+      await sleepAsync(1.seconds);
+      final todoList = await client.getTodoList();
+      return SimpleResult.success(todoList);
+    });
+  }
+
+  @override
+  Future<SimpleResult<void, ApiError>> addTodo(Todo todo) async {
+    return tryRequest(() async {
+      await client.addTodo(todo);
+      return SimpleResult.success();
+    });
+  }
+
+  @override
+  Future<SimpleResult<void, ApiError>> updateTodo(Todo todo) async {
+    return tryRequest(() async {
+      await client.updateTodo(todo.id, todo);
+      return SimpleResult.success();
+    });
+  }
+
+  @override
+  Future<SimpleResult<void, ApiError>> removeTodo(int id) async {
+    return tryRequest(() async {
+      await client.removeTodo(id);
+      return SimpleResult.success();
+    });
+  }
+
+  Future<SimpleResult<T, ApiError>> tryRequest<T>(
+      Future<SimpleResult<T, ApiError>> Function() apiLogic) async {
+    try {
+      return await apiLogic();
+    } catch (e) {
+      return ApiError.createErrorResult(e);
+    }
+  }
+}
